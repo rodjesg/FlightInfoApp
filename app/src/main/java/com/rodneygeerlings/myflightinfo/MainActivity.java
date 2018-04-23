12fcf4472b9410cf2b0d5ab3f7bb3c8507e962e8
Rodney Geerlings <rodney@rgeerlings.com>
2018-04-23 23:26:48 +0200

    Structure & Update

--- app/src/main/java/com/rodneygeerlings/myflightinfo/MainActivity.java
+++ app/src/main/java/com/rodneygeerlings/myflightinfo/MainActivity.java
@@ -1,15 +1,53 @@
 package com.rodneygeerlings.myflightinfo;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.rodneygeerlings.myflightinfo.R;
+import com.rodneygeerlings.myflightinfo.activities.ContactActivity;
+import com.rodneygeerlings.myflightinfo.activities.FlightActivity;
+import com.rodneygeerlings.myflightinfo.activities.WeatherActivity;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                Button contactInfo = findViewById(R.id.contactInfo);
+                Button weatherInfo = findViewById(R.id.weatherInfo);
+                Button flightInfo = findViewById(R.id.flightInfo);
+
+                contactInfo.setOnClickListener(this);
+                weatherInfo.setOnClickListener(this);
+                flightInfo.setOnClickListener(this);
+
+            }
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.contactInfo:
+                        Intent intent = new Intent(this, ContactActivity.class);
+                        startActivity(intent);
+
+                        break;
+                    case R.id.weatherInfo:
+                        Intent intent2 = new Intent(this, WeatherActivity.class);
+                        startActivity(intent2);
+
+                        break;
+                    case R.id.flightInfo:
+                        Intent intent3 = new Intent(this, FlightActivity.class);
+                        startActivity(intent3);
+
+                        break;
+
+                }
+            }
+        }
---
+++ app/src/main/java/com/rodneygeerlings/myflightinfo/activities/FlightActivity.java
@@ -0,0 +1,16 @@
+package com.rodneygeerlings.myflightinfo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rodneygeerlings.myflightinfo.R;
+
+
+public class FlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight);
+    }
+}
